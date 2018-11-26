dce9ad8063b9d3dc30f36d8a9a84775317a84b2c
John Junkins <jj@j6srvs.com>
2018-11-25 22:27:27 -0600

    messing around with netlifly cli

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/home/john/Documents/Projects/gitlab/peronalsite/node_modules/gatsby-plugin-netlify-cms/gatsby-browser'),
-      options: {"plugins":[],"modulePath":"/home/john/Documents/Projects/gitlab/peronalsite/src/cms/cms.js"},
-    }]
--- public/component---src-pages-404-js-6322a72eab8b03d3abdc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{140:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(149);t.default=function(){return l.a.createElement(u.a,null,l.a.createElement("div",null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6322a72eab8b03d3abdc.js.map
