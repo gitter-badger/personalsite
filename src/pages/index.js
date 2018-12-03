e6d8309d3f9496b1c5edde285798c85dba3c3fc0
John Junkins <jj@j6srvs.com>
2018-12-03 10:50:22 -0600

    Updating DEV Branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Testing Build Script </h1>
+              <h1 className="has-text-weight-bold is-size-2">Updating DEV Branch</h1>
             </div>
             {posts.map(({ node: post }) => (
               <div
