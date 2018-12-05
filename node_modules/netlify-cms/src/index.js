e2d76ff1a7dacca60c726c96da26b313427da794
John Junkins <jj@j6srvs.com>
2018-12-05 13:50:19 -0600

    node error

--- node_modules/netlify-cms/src/index.js
+++
@@ -1,7 +0,0 @@
-import CMS, { init } from 'netlify-cms-core/src';
-import './backends';
-import './widgets';
-import './editor-components';
-import './media-libraries';
-
-export { CMS as default, init };
--- node_modules/netlify-cms/src/media-libraries.js
+++
@@ -1,6 +0,0 @@
-import cms from 'netlify-cms-core/src';
-import uploadcare from 'netlify-cms-media-library-uploadcare/src';
-
-const { registerMediaLibrary } = cms;
-
-registerMediaLibrary(uploadcare);
