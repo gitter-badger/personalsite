bc59c2135f06f221667df860143a9b22f549514f
John <junkins.jj@gmail.com>
2019-01-23 11:51:50 -0600

    fix conflict windows

--- src/pages/tags/index.js
+++ src/pages/tags/index.js
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { Link, graphql } from 'gatsby'
-import Layout from '../../components/Layout'
-// QUESTION: How does Gatsby or Netlifly CMS or GraphQL store Tags? 
-const TagsPage = ({
-  data: { allMarkdownRemark: { group }, site: { siteMetadata: { title } } },
-}) => (
-  <Layout>
-    <section className="section">
-      <Helmet title={`Tags | ${title}`} />
-      <div className="container content">
-        <div className="columns">
-          <div
-            className="column is-10 is-offset-1"
-            style={{ marginBottom: '6rem' }}
-          >
-            <h1 className="title is-size-2 is-bold-light">Tags</h1>
-            <ul className="taglist">
-              {group.map(tag => (
-                <li key={tag.fieldValue}>
-                  <Link to={`/tags/${kebabCase(tag.fieldValue)}/`}>
-                    {tag.fieldValue} ({tag.totalCount})
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </section>
-  </Layout>
-)
-
-export default TagsPage
-
-export const tagPageQuery = graphql`
-  query TagsQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(limit: 1000) {
-      group(field: frontmatter___tags) {
-        fieldValue
-        totalCount
-      }
-    }
-  }
-`
-=======
 import React from 'react'
 import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
@@ -104,4 +50,3 @@ export const tagPageQuery = graphql`
     }
   }
 `
->>>>>>> workflow
